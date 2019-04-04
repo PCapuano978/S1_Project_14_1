33b43ef81cd49dc57a87b9379486188ab84dcaf4
Unknown <pcapuano978@west-mec.org>
2019-04-03 22:45:40 -0700

    Project One Finished; Ready For Review
    Documentation has been finished in relation to the rest of the code. Luckily enough, the website still works as intended, marking this one whole and complete for review.

--- na_styler.js
+++ na_styler.js
@@ -37,30 +37,64 @@ function setStyles() {
       id attribute set to fancySheet,
       rel attribute set to stylesheet,
       and href attribute set to "na_style_" with the styleNum value ending with ".css"*/
-
       linkElement.setAttribute("id", "fancySheet");
       linkElement.setAttribute("rel", "stylesheet");
       linkElement.setAttribute("href", "na_style_" + styleNum + ".css");
 
+      // The child node of "linkElement" is to be appended to the head element.
       document.head.appendChild(linkElement);
 
+      // The "figBox" variable is declared with it being set to a figure node created within the document.
       var figBox = document.createElement("figure")
 
+      // The figBox's id attribute is set to "styleThumbs".
       figBox.setAttribute("id", "styleThumbs");
 
+      // The figBox child node is appended in the elements of HTML with the id of "box".
       document.getElementById("box").appendChild(figBox);
 
-
+      // for loop starts cycling numbers one through four.
       for (var i = 0; i < 4; i++) {
 
+            // "sheetImg" is declared and set to be an image node.
             var sheetImg = document.createElement("img");
 
+            // sheetImg has two attribute of source and alt that are set to text strings including index numbers of the collection.
             sheetImg.setAttribute("src", "na_small_" + [i] + ".png");
             sheetImg.setAttribute("alt", "na_style_" + [i] + ".css");
 
-
-
+           
+            
+            
       };
+
+      // "thumbStyles" is declared where it is made into a stylesheer node.
+      var thumbStyles = document.createElement("style");
+
+            // thumbStyles is appended to the document head.
+            document.head.appendChild(thumbStyles);
+
+            // Three stylesheet rule are embeeded in here through the thumbStlye node and includes the following rules.
+            document.styleSheet[document.styleSheets.length - 1].insertRule(
+                  "figure#styleThumbs { \
+                  position: absolute; \
+                  left: 0px; \
+                  bottom: 0px; \
+                  }", 0);
+
+
+            document.styleSheets[document.styleSheets.length - 1].insertRule(
+                  "figure#styleThumbs img { \
+                  outline: 1px solid black; \
+                  cursor: pointer; \
+                  opacity: 0.75; \
+                  }", 1);
+
+            document.styleSheets[document.styleSheets.length - 1].insertRule(
+            "figure#styleThumbs img:hover { \
+                  outline: 1px solid red; \
+                  opacity: 1.0; \
+                  }", 2);
 
 
 }
